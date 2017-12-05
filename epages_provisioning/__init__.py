605b0aaf8ea0248a0f66c765de457cc63b793448
Tatu Wikman <tatu.wikman@gmail.com>
2017-12-05 18:10:03 +0200

    something that works

--- epages_provisioning/__init__.py
+++ epages_provisioning/__init__.py
@@ -5,3 +5,8 @@
 __author__ = """Tatu Wikman"""
 __email__ = 'tatu.wikman@gmail.com'
 __version__ = '0.1.0'
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from .provisioning import *
--- tests/test_epages_provisioning.py
+++ tests/test_epages_provisioning.py
@@ -3,20 +3,50 @@
 
 """Tests for `epages_provisioning` package."""
 
-
+import os
 import unittest
 
-from epages_provisioning import epages_provisioning
+from epages_provisioning import provisioning
 
 
-class TestEpages_provisioning(unittest.TestCase):
-    """Tests for `epages_provisioning` package."""
+class TestSimpleProvisioning(unittest.TestCase):
+    """ Tests for `epages_provisioning` package.
+    All tests require that the environment variables:
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+        EP_ENDPOINT
+        EP_PROVIDER
+        EP_USERNAME
+        EP_PASSWORD
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    are set
 
-    def test_000_something(self):
-        """Test something."""
+    For example:
+
+        EP_ENDPOINT=http://tatu05.sml18.vilkas.pri/epages/Site.soap EP_PROVIDER=Distributor EP_USERNAME=admin EP_PASSWORD=admin make test
+
+    These tests also assume that the ePages service is a default installation with default shoptypes etc.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """ set up our client for the tests """
+        cls._sp = provisioning.SimpleProvisioningService(
+            endpoint=os.environ['EP_ENDPOINT'],
+            provider=os.environ['EP_PROVIDER'],
+            username=os.environ['EP_USERNAME'],
+            password=os.environ['EP_PASSWORD'],
+            )
+
+    def test_000_create(self):
+        """ test creating new shop """
+        data = {
+            'Alias': 'foobar',
+            'ShopType': 'MinDemo',
+        }
+        self._sp.create(data)
+
+    def test_001_delete(self):
+        data = {
+            'Alias': 'foobar',
+        }
+        self._sp.markForDeletion(data)
