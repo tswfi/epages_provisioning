5de1d05e8f037e167fb006e9b04a6dea824ebe25
Tatu Wikman <tatu.wikman@gmail.com>
2017-12-08 17:06:00 +0200

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='epages_provisioning',
-    version='0.1.6',
+    version='0.1.7',
     description="Python library for calling ePages provisioning services",
     long_description=readme + '\n\n' + history,
     author="Tatu Wikman",
