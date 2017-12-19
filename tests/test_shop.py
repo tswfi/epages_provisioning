02665c367b082821513425605137f8b451fd2600
Tatu Wikman <tatu.wikman@gmail.com>
2017-12-19 18:05:13 +0200

    initial tests

---
+++ tests/test_shop.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `epages_provisioning` package."""
+
+import os
+import unittest
+from datetime import datetime
+
+from epages_provisioning.provisioning import ShopConfigService
+from epages_provisioning.shop import Shop
+
+# check for our environment variables
+envcheck = unittest.skipUnless(
+    os.environ.get('EP_SERVER', False) and
+    os.environ.get('EP_PROVIDER', False) and
+    os.environ.get('EP_PASSWORD', False) and
+    os.environ.get('EP_USERNAME', False), 'Environment variables not set'
+)
+
+
+@envcheck
+class TestShop(unittest.TestCase):
+    """ Tests for `epages_provisioning` shop Class.
+    All tests require that the environment variables:
+
+        EP_SERVER
+        EP_PROVIDER
+        EP_USERNAME
+        EP_PASSWORD
+
+    are set
+
+    For example:
+
+        export EP_SERVER=http://example.com/
+        export EP_PROVIDER=Distributor
+        export EP_USERNAME=admin
+        export EP_PASSWORD=admin
+        make test
+
+    These tests also assume that the ePages service is a default installation
+    with default shoptypes etc.
+
+    Also if you want to see a full message trace say:
+
+        export EP_TRACE=1
+
+    and then run your tests
+
+    Warning: these will take a while to run.
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        set up our client for the tests
+        """
+        cls._sc = ShopConfigService(
+            server=os.environ['EP_SERVER'],
+            provider=os.environ['EP_PROVIDER'],
+            username=os.environ['EP_USERNAME'],
+            password=os.environ['EP_PASSWORD'],
+        )
+        # used as alias for these tests
+        cls._nowstr = datetime.now().strftime('%Y%m%d%H%M%S%f')
+        cls._alias = 'test-{}-min'.format(cls._nowstr)
+        cls._shoptype = 'MinDemo'
+
+    def setUp(self):
+        """ get a shop to play with """
+        self.s = Shop(Alias=self._alias, provisioning=self._sc)
+
+    def test_001_start(self):
+        self.assertTrue(self.s)
+
+    def test_010_create(self):
+        """ set the shop type and create the shop """
+        print(self.s)
+        self.s.ShopType = self._shoptype
+        self.assertIsNone(self.s.create())
+        self.assertEqual(self.s.Alias, self._alias)
+
+        self.assertFalse(self.s.IsTrialShop)
+        self.assertFalse(self.s.IsClosed)
+        self.assertFalse(self.s.IsClosedTemporarily)
